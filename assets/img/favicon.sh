2a0e94f3e0598434146459f5a6eb04d50daf19bd
vjrj <vjrj@comunes.org>
2023-03-05 16:52:18 +0100

    Better favicon generation

--- assets/img/favicon.sh
+++ assets/img/favicon.sh
@@ -6,5 +6,9 @@ convert gbrevedot.png -resize 256x256 \
         -define icon:auto-resize="256,128,96,64,48,32,16" \
         favicon.ico
 
+convert gbrevedot.png -resize 256x256 favicon.png
+
+cp favicon.png ../../web/
+
 for i in 192 512; do convert gbrevedot.png -resize $ix$i ../../web/icons/Icon-$i.png; done
 for i in 192 512; do convert gbrevedot.png -resize $ix$i ../../web/icons/Icon-maskable-$i.png; done

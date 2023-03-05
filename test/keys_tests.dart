ce148bcf2106aa3bb9bc5367edf2f84b8d22bd5f
vjrj <vjrj@comunes.org>
2023-03-05 15:47:08 +0100

    Package rename and remove old deps

--- test/keys_tests.dart
+++ test/keys_tests.dart
@@ -2,7 +2,7 @@ import 'dart:typed_data';
 
 import 'package:durt/durt.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mondamono/g1/keys_helper.dart';
+import 'package:ginkgo/g1/keys_helper.dart';
 
 void main() {
   test('Test serialization and deserialization of UInt8List seeds', () {

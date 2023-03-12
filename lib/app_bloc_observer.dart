1cf7624c89fead162166c4bfce17f3676884ffbd
vjrj <vjrj@comunes.org>
2023-03-12 23:30:23 +0100

    Added log to error in bloc observer

---
+++ lib/app_bloc_observer.dart
@@ -0,0 +1,37 @@
+import 'package:bloc/bloc.dart';
+
+import 'main.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase<dynamic> bloc) {
+    super.onCreate(bloc);
+    logger('onCreate -- ${bloc.runtimeType}');
+  }
+
+  @override
+  void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
+    super.onEvent(bloc, event);
+    logger('onEvent -- $event');
+  }
+
+  @override
+  void onTransition(
+      Bloc<dynamic, dynamic> bloc, Transition<dynamic, dynamic> transition) {
+    super.onTransition(bloc, transition);
+    logger('onTransition -- $transition');
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    logger('onError -- $error');
+    logger(stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase<dynamic> bloc) {
+    super.onClose(bloc);
+    logger('onClose -- ${bloc.runtimeType}');
+  }
+}

96a0ed73de143060ffdd8f7f011d67c486473177
vjrj <vjrj@comunes.org>
2023-04-26 19:54:03 +0200

    Fix lint warning

--- lib/ui/tutorial.dart
+++ lib/ui/tutorial.dart
@@ -1,10 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ginkgo/ui/ui_helpers.dart';
 import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
 import '../data/models/app_cubit.dart';
+import 'ui_helpers.dart';
 
 abstract class Tutorial {
   Tutorial({

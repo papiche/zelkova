87a3c19660c9882a633e21d9775dfcf1f5296280
vjrj <vjrj@comunes.org>
2024-07-21 14:01:21 +0200

    Added new repo

--- lib/g1/g1_v2_helper_web.dart
+++ lib/g1/g1_v2_helper_web.dart
@@ -1,3 +1,5 @@
 // While polkadart doesn't support web
 // https://github.com/leonardocustodio/polkadart/issues/297
+// See too:
+// https://github.com/mrtnetwork/polkadot_dart
 String addressFromV1Pubkey(String pubKey) => '';

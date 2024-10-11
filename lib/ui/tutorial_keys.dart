2cdb7bf7c25de33a3e36797772fe11d569c8b267
vjrj <vjrj@comunes.org>
2024-10-11 22:50:27 +0200

    Added debugLabel to Globalkey to debug dup key

--- lib/ui/tutorial_keys.dart
+++ lib/ui/tutorial_keys.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 
 // first screen
-final GlobalKey creditCardKey = GlobalKey();
-final GlobalKey creditCardPubKey = GlobalKey();
-final GlobalKey paySearchUserKey = GlobalKey();
-final GlobalKey payAmountKey = GlobalKey();
-final GlobalKey paySentKey = GlobalKey();
+final GlobalKey creditCardKey = GlobalKey(debugLabel: 'creditCardPubKey');
+final GlobalKey creditCardPubKey = GlobalKey(debugLabel: 'creditCardPubKey');
+final GlobalKey paySearchUserKey = GlobalKey(debugLabel: 'paySearchUserKey');
+final GlobalKey payAmountKey = GlobalKey(debugLabel: 'payAmountKey');
+final GlobalKey paySentKey = GlobalKey(debugLabel: 'paySentKey');
 
 // second screen
-final GlobalKey receiveMainKey = GlobalKey();
-final GlobalKey receiveQrKey = GlobalKey();
-final GlobalKey receiveAmountKey = GlobalKey();
-final GlobalKey receiveSumKey = GlobalKey();
+final GlobalKey receiveMainKey = GlobalKey(debugLabel: 'receiveMainKey');
+final GlobalKey receiveQrKey = GlobalKey(debugLabel: 'receiveQrKey');
+final GlobalKey receiveAmountKey = GlobalKey(debugLabel: 'receiveAmountKey');
+final GlobalKey receiveSumKey = GlobalKey(debugLabel: 'receiveSumKey');
 
 // third screen
-final GlobalKey contactsMainKey = GlobalKey();
-final GlobalKey contactsQrKey = GlobalKey();
+final GlobalKey contactsMainKey = GlobalKey(debugLabel: 'contactsMainKey');
+final GlobalKey contactsQrKey = GlobalKey(debugLabel: 'contactsQrKey');
 
 // fourth screen
-final GlobalKey txMainKey = GlobalKey();
-final GlobalKey txRefreshKey = GlobalKey();
-final GlobalKey txBalanceKey = GlobalKey();
+final GlobalKey txMainKey = GlobalKey(debugLabel: 'txMainKey');
+final GlobalKey txRefreshKey = GlobalKey(debugLabel: 'txRefreshKey');
+final GlobalKey txBalanceKey = GlobalKey(debugLabel: 'txBalanceKey');
 
 // fifth screen
-final GlobalKey infoMainKey = GlobalKey();
-final GlobalKey exportMainKey = GlobalKey();
+final GlobalKey infoMainKey = GlobalKey(debugLabel: 'infoMainKey');
+final GlobalKey exportMainKey = GlobalKey(debugLabel: 'exportMainKey');

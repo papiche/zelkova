f641edbcd00b0872b1a56d0ad53d10681ee465f4
vjrj <vjrj@comunes.org>
2024-11-16 10:46:53 +0100

    Add file_selector_linux and flutter_zxing plugins

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -5,11 +5,13 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   awesome_notifications
   file_saver
+  file_selector_linux
   sentry_flutter
   url_launcher_linux
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  flutter_zxing
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)

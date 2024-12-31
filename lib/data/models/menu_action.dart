a787acc2fc6a3d14ef9aae6722790d79cdd7895c
vjrj <vjrj@comunes.org>
2024-12-31 19:49:39 +0100

    Update MenuAction model to return SignAndSendResult

--- lib/data/models/menu_action.dart
+++ lib/data/models/menu_action.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../../g1/sing_and_send.dart';
+
 class MenuAction {
   MenuAction({required this.name, required this.icon, required this.action});
 
   final String name;
   final IconData icon;
-  final Future<String> Function() action;
+  final Future<SignAndSendResult> Function() action;
 }

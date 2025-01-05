da16d7677be74c4241e3c9cfd73782e25f26cb6b
vjrj <vjrj@comunes.org>
2025-01-05 12:39:23 +0100

    Use app_link instead of http_link

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:app_links/app_links.dart';
 import 'package:connectivity_wrapper/connectivity_wrapper.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:ferry_hive_store/ferry_hive_store.dart';
@@ -24,7 +25,6 @@ import 'package:responsive_framework/responsive_framework.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:sentry_logging/sentry_logging.dart';
 import 'package:timeago/timeago.dart' as timeago;
-import 'package:uni_links3/uni_links.dart';
 import 'package:workmanager/workmanager.dart';
 
 import 'app_bloc_observer.dart';
@@ -505,9 +505,10 @@ class _GinkgoAppState extends State<GinkgoApp> {
   StreamSubscription<dynamic>? _sub;
 
   Future<void> _initDeepLinkListener() async {
-    // TODO(vjrj): do the same for IOs
-    if (Platform.isAndroid || kIsWeb) {
-      _sub = linkStream.listen((String? link) async {
+    // TODO(vjrj): Configure properly Windows
+    if (Platform.isAndroid || kIsWeb || Platform.isLinux || Platform.isIOS) {
+      final AppLinks appLinks = AppLinks(); // AppLinks is singleton
+      _sub = appLinks.stringLinkStream.listen((String? link) async {
         if (!mounted) {
           return;
         }

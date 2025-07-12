0d18624ff9bac58efb105cc4e7c9883d1089d97a
vjrj <vjrj@comunes.org>
2025-07-12 12:32:56 +0200

    Add StorageKeys class with constants for storage keys

---
+++ lib/storage_keys.dart
@@ -0,0 +1,13 @@
+class StorageKeys {
+  static const String seedKey = 'seed';
+  static const String pubKey = 'pub';
+  static const String cardsKey = 'cesiumCards';
+  static const String currentCardIndexKey = 'current_wallet_index';
+  static const String securePatternOrPass =
+      'secure_storage_pattern_or_pass_key'; // V2
+  static const String secureSalt = 'secure_storage_salt'; // V2
+  static const String usesPassword = 'storage_uses_password'; // V2
+
+  static const String accountsKey = 'secure_storage_accounts'; // V2
+  static const String biometricEnabledKey = 'auth_enable_biometrics'; // V2
+}

5d60945bf9d60dd3698b44eb467cff4e73cfeb32
vjrj <vjrj@comunes.org>
2025-07-12 12:34:20 +0200

    Refactor keypair generation and mnemonic handling

--- test/g1_v2_test.dart
+++ test/g1_v2_test.dart
@@ -4,7 +4,6 @@ import 'dart:typed_data';
 import 'package:bip39_multi_nullsafety/src/wordlists/spanish.dart' as spanish;
 import 'package:durt/src/crypto/cesium_wallet.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ginkgo/data/models/auth_data.dart';
 import 'package:ginkgo/g1/g1_v2_helper.dart';
 import 'package:ginkgo/ui/logger.dart';
 import 'package:polkadart_keyring/polkadart_keyring.dart';
@@ -67,49 +66,49 @@ void main() {
     final Uint8List seed = wallet.seed;
     final Keyring keyring = Keyring();
     final KeyPair keypair =
-        keyring.fromSeed(seed, keyPairType: KeyPairType.ed25519);
+    keyring.fromSeed(seed, keyPairType: KeyPairType.ed25519);
     expect(keypair.address, equals(v2Address));
   });
 
   test(
       'generate a keypair via mnemonic and do test with addresses and v1 pubkeys ',
-      () async {
-    //const String devMnemonic =
-    //    'bottom drive obey lake curtain smoke basket hold race lonely fit walk';
-    const String devMnemonic =
-        'drama dream insane parrot train corn steak latin voice extend fragile concert';
-    final Keyring devKeyring = Keyring();
-    final KeyPair devKeyPairEd = await devKeyring.fromMnemonic(devMnemonic,
-        keyPairType: KeyPairType.ed25519);
-    final KeyPair devKeyPairSr = await devKeyring.fromMnemonic(devMnemonic,
-        keyPairType: KeyPairType.sr25519);
-    const String expectedV2DevAddressSr =
-        '5GTy25GQxWinyhAafiwfVqNV3qFCwJkpbcpzkwKu3qk8HoJN';
-    const String expectedV2DevAddressEd =
-        '5HEwSt1D87Hx161vrqmuCy975y2TFMRqpbja6DcQUNkBLL5b';
-    expect(devKeyPairEd.address, equals(expectedV2DevAddressEd));
-    expect(devKeyPairSr.address, equals(expectedV2DevAddressSr));
-    expect(isValidV2Address(expectedV2DevAddressEd), true);
-    const String expectedV1PubKeySr =
-        'E6xXZNFciyNfdgqcY7QUm1vQcBgPP3dQZegcj8GGSYgV';
-    const String expectedV1PubKeyEd =
-        'GQrGV3TeEVvA22jtaEUseyEUMajxgsbGPbE5kNpGi3wi';
-    expect(v1pubkeyFromAddress(expectedV2DevAddressEd),
-        equals(expectedV1PubKeyEd));
-    expect(v1pubkeyFromAddress(expectedV2DevAddressSr),
-        equals(expectedV1PubKeySr));
+          () async {
+        //const String devMnemonic =
+        //    'bottom drive obey lake curtain smoke basket hold race lonely fit walk';
+        const String devMnemonic =
+            'drama dream insane parrot train corn steak latin voice extend fragile concert';
+        final Keyring devKeyring = Keyring();
+        final KeyPair devKeyPairEd = await devKeyring.fromMnemonic(devMnemonic,
+            keyPairType: KeyPairType.ed25519);
+        final KeyPair devKeyPairSr = await devKeyring.fromMnemonic(devMnemonic,
+            keyPairType: KeyPairType.sr25519);
+        const String expectedV2DevAddressSr =
+            '5GTy25GQxWinyhAafiwfVqNV3qFCwJkpbcpzkwKu3qk8HoJN';
+        const String expectedV2DevAddressEd =
+            '5HEwSt1D87Hx161vrqmuCy975y2TFMRqpbja6DcQUNkBLL5b';
+        expect(devKeyPairEd.address, equals(expectedV2DevAddressEd));
+        expect(devKeyPairSr.address, equals(expectedV2DevAddressSr));
+        expect(isValidV2Address(expectedV2DevAddressEd), true);
+        const String expectedV1PubKeySr =
+            'E6xXZNFciyNfdgqcY7QUm1vQcBgPP3dQZegcj8GGSYgV';
+        const String expectedV1PubKeyEd =
+            'GQrGV3TeEVvA22jtaEUseyEUMajxgsbGPbE5kNpGi3wi';
+        expect(v1pubkeyFromAddress(expectedV2DevAddressEd),
+            equals(expectedV1PubKeyEd));
+        expect(v1pubkeyFromAddress(expectedV2DevAddressSr),
+            equals(expectedV1PubKeySr));
 
-    final List<int> devSeed =
+        final List<int> devSeed =
         await SubstrateBip39.ed25519.seedFromUri(devMnemonic);
-    final Uint8List devSeedU8a = Uint8List.fromList(devSeed);
-    final CesiumWallet wallet2 = CesiumWallet.fromSeed(devSeedU8a);
-    expect(wallet2.pubkey, equals(expectedV1PubKeyEd));
-  });
+        final Uint8List devSeedU8a = Uint8List.fromList(devSeed);
+        final CesiumWallet wallet2 = CesiumWallet.fromSeed(devSeedU8a);
+        expect(wallet2.pubkey, equals(expectedV1PubKeyEd));
+      });
 
   // Disabled as polkadot does not support spanish mnemonics
   test('Spanish bit39', skip: true, () async {
     final List<String> esMnemonicList =
-        mnemonicGenerate(lang: 'spanish').split(' ');
+    mnemonicGenerate(lang: 'spanish').split(' ');
 
     final Keyring keyring = Keyring();
     final String mnemonic = esMnemonicList.join(' ');
@@ -136,19 +135,5 @@ void main() {
 
     final bool isVerified = keypair.verify(message, signature);
     expect(isVerified, true);
-  });
-
-  test('createPair and AuthData', () async {
-    const String password = 'devtest';
-    // const String nm ='abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about';
-
-    final AuthDataV1 authDataV1 = AuthDataV1(password, password);
-    // final AuthDataV2 authDataV2 = AuthDataV2(nm, password);
-    final CesiumWallet walletV1 = CesiumWallet(password, password);
-
-    final KeyPair keyPair =
-        await createPair(AuthData(v1: authDataV1), Keyring());
-
-    expect(v1pubkeyFromAddress(keyPair.address), walletV1.pubkey);
   });
 }

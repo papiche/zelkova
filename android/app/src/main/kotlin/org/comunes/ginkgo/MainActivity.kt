4661086cec67eb4b54fa94db4c7f8a8f8876151e
vjrj <vjrj@comunes.org>
2025-08-17 20:36:11 +0200

    Update workmanager

--- android/app/src/main/kotlin/org/comunes/ginkgo/MainActivity.kt
+++ android/app/src/main/kotlin/org/comunes/ginkgo/MainActivity.kt
@@ -1,6 +1,14 @@
 package org.comunes.ginkgo
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterFragmentActivity
+import dev.fluttercommunity.workmanager.WorkmanagerDebug
+import dev.fluttercommunity.workmanager.NotificationDebugHandler
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity : FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WorkmanagerDebug.setCurrent(NotificationDebugHandler())
+    }
 }
+

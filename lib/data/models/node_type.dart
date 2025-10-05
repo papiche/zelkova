de650d1f908786d4a18bb06a787d6e6dde39f550
vjrj <vjrj@comunes.org>
2025-10-05 17:54:39 +0200

    Add NodeType extension to determine if node is v2 type

--- lib/data/models/node_type.dart
+++ lib/data/models/node_type.dart
@@ -7,3 +7,20 @@ enum NodeType {
   datapodEndpoint,
   ipfsGateway
 }
+
+// Extension to know if node is v2 type
+extension NodeTypeExtension on NodeType {
+  bool get isV2 {
+    switch (this) {
+      case NodeType.duniter:
+      case NodeType.cesiumPlus:
+      case NodeType.gva:
+        return false;
+      case NodeType.endpoint:
+      case NodeType.duniterIndexer:
+      case NodeType.datapodEndpoint:
+      case NodeType.ipfsGateway:
+        return true;
+    }
+  }
+}

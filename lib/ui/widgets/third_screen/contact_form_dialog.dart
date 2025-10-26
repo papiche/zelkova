a2402028aa8355128f2dc4d088194ae9ecd69542
vjrj <vjrj@comunes.org>
2025-10-26 23:24:26 +0100

    Update contact form dialog layout and functionality to allow edit in small screens (thanks @MaAude91 for the report)

--- lib/ui/widgets/third_screen/contact_form_dialog.dart
+++ lib/ui/widgets/third_screen/contact_form_dialog.dart
@@ -52,107 +52,110 @@ class _ContactFormDialogState extends State<ContactFormDialog> {
     return AlertDialog(
       title: Text(
           tr(widget.isNew ? 'form_contact_title_add' : 'form_contact_title')),
-      content: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            if (!widget.isNew)
-              Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-                Flexible(
-                    child: TextFormField(
-                  // maxLines: 2,
-                  initialValue: humanizePubKey(_updatedContact.pubKey),
-                  style: keyStyle,
-                  decoration: keyDecoration.copyWith(
-                    labelText: tr('form_contact_pub_key'),
-                  ),
-                  enabled: false,
-                )),
-                GestureDetector(
-                  onTap: () {
-                    showQrDialog(
-                        context: context,
-                        pubKeyOrAddress: _updatedContact.pubKey,
-                        isV2: false,
-                        noTitle: true,
-                        feedbackText: 'some_key_copied_to_clipboard');
-                  },
-                  child: const Icon(Icons.qr_code, size: 50),
-                ),
-              ]),
-            if (!widget.isNew) const SizedBox(height: 5),
-            if (!widget.isNew)
-              Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-                Flexible(
-                    child: TextFormField(
-                  // maxLines: 2,
-                  initialValue: humanizePubKey(_updatedContact.address),
-                  decoration: keyDecoration.copyWith(
-                    labelText: tr('form_contact_address_v2'),
-                  ),
-                  style: keyStyle,
-                  enabled: false,
-                )),
-                GestureDetector(
+      content: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              if (!widget.isNew)
+                Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                  Flexible(
+                      child: TextFormField(
+                    // maxLines: 2,
+                    initialValue: humanizePubKey(_updatedContact.pubKey),
+                    style: keyStyle,
+                    decoration: keyDecoration.copyWith(
+                      labelText: tr('form_contact_pub_key'),
+                    ),
+                    enabled: false,
+                  )),
+                  GestureDetector(
                     onTap: () {
                       showQrDialog(
                           context: context,
-                          isV2: true,
-                          pubKeyOrAddress: _updatedContact.address,
+                          pubKeyOrAddress: _updatedContact.pubKey,
+                          isV2: false,
                           noTitle: true,
                           feedbackText: 'some_key_copied_to_clipboard');
                     },
-                    child: const Badge(
-                      label: Text('v2'),
-                      child: Icon(Icons.qr_code, size: 50),
-                    )),
-              ]),
-            if (widget.isNew)
+                    child: const Icon(Icons.qr_code, size: 50),
+                  ),
+                ]),
+              if (!widget.isNew) const SizedBox(height: 5),
+              if (!widget.isNew)
+                Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                  Flexible(
+                      child: TextFormField(
+                    // maxLines: 2,
+                    initialValue: humanizePubKey(_updatedContact.address),
+                    decoration: keyDecoration.copyWith(
+                      labelText: tr('form_contact_address_v2'),
+                    ),
+                    style: keyStyle,
+                    enabled: false,
+                  )),
+                  GestureDetector(
+                      onTap: () {
+                        showQrDialog(
+                            context: context,
+                            isV2: true,
+                            pubKeyOrAddress: _updatedContact.address,
+                            noTitle: true,
+                            feedbackText: 'some_key_copied_to_clipboard');
+                      },
+                      child: const Badge(
+                        label: Text('v2'),
+                        child: Icon(Icons.qr_code, size: 50),
+                      )),
+                ]),
+              if (widget.isNew)
+                TextFormField(
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty || !validateKey(value)) {
+                      return tr('form_contact_name_validation_pub_key');
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: tr('form_contact_pub_or_address'),
+                  ),
+                  onChanged: (String? value) {
+                    if (value != null) {
+                      final bool isV2Address = isValidV2Address(value);
+                      final String pubKey =
+                          isV2Address ? v1pubkeyFromAddress(value) : value;
+                      final String address =
+                          isV2Address ? value : addressFromV1Pubkey(value);
+                      _updatedContact = _updatedContact.copyWith(
+                          pubKey: pubKey, address: address);
+                    }
+                  },
+                ),
+              if (!widget.isNew) const SizedBox(height: 5),
               TextFormField(
+                initialValue: _updatedContact.name,
+                decoration: InputDecoration(labelText: tr('form_contact_name')),
                 validator: (String? value) {
-                  if (value == null || value.isEmpty || !validateKey(value)) {
-                    return tr('form_contact_name_validation_pub_key');
+                  if (value == null || value.isEmpty) {
+                    return tr('form_contact_name_validation');
                   }
                   return null;
                 },
-                decoration: InputDecoration(
-                  labelText: tr('form_contact_pub_or_address'),
-                ),
                 onChanged: (String? value) {
-                  if (value != null) {
-                    final bool isV2Address = isValidV2Address(value);
-                    final String pubKey =
-                        isV2Address ? v1pubkeyFromAddress(value) : value;
-                    final String address =
-                        isV2Address ? value : addressFromV1Pubkey(value);
-                    _updatedContact = _updatedContact.copyWith(
-                        pubKey: pubKey, address: address);
-                  }
+                  _updatedContact = _updatedContact.copyWith(name: value);
                 },
               ),
-            if (!widget.isNew) const SizedBox(height: 5),
-            TextFormField(
-              initialValue: _updatedContact.name,
-              decoration: InputDecoration(labelText: tr('form_contact_name')),
-              validator: (String? value) {
-                if (value == null || value.isEmpty) {
-                  return tr('form_contact_name_validation');
-                }
-                return null;
-              },
-              onChanged: (String? value) {
-                _updatedContact = _updatedContact.copyWith(name: value);
-              },
-            ),
-            TextFormField(
-              initialValue: _updatedContact.notes,
-              decoration: InputDecoration(labelText: tr('form_contact_notes')),
-              onChanged: (String? value) {
-                _updatedContact = _updatedContact.copyWith(notes: value);
-              },
-            ),
-          ],
+              TextFormField(
+                initialValue: _updatedContact.notes,
+                decoration:
+                    InputDecoration(labelText: tr('form_contact_notes')),
+                onChanged: (String? value) {
+                  _updatedContact = _updatedContact.copyWith(notes: value);
+                },
+              ),
+            ],
+          ),
         ),
       ),
       actions: <Widget>[

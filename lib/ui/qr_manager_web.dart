d2498214e24479284e0dbd104662cce4b5b9cfae
vjrj <vjrj@comunes.org>
2025-11-08 11:49:13 +0100

    Remove universal_html

--- lib/ui/qr_manager_web.dart
+++ lib/ui/qr_manager_web.dart
@@ -1,23 +1,27 @@
 import 'dart:async';
+import 'dart:js_interop';
 import 'dart:typed_data';
 import 'dart:ui_web' as ui_web;
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:universal_html/html.dart';
+import 'package:web/web.dart' as web;
 import 'package:zxing2/qrcode.dart';
 
 mixin QrManager {
   static Future<String?> qrScan(BuildContext context) async {
-    final VideoElement video = VideoElement();
+    final web.HTMLVideoElement video = web.HTMLVideoElement();
     final String viewId = 'qr-video-${DateTime.now().millisecondsSinceEpoch}';
 
     ui_web.platformViewRegistry.registerViewFactory(viewId, (int _) => video);
 
-    final MediaStream stream =
-        await window.navigator.mediaDevices!.getUserMedia(<dynamic, dynamic>{
-      'video': <String, String>{'facingMode': 'environment'}
-    });
+    final web.MediaStreamConstraints constraints = web.MediaStreamConstraints(
+      video: <String, dynamic>{'facingMode': 'environment'}.jsify()!,
+    );
+
+    final web.MediaStream stream = await web.window.navigator.mediaDevices
+        .getUserMedia(constraints)
+        .toDart;
 
     video
       ..srcObject = stream
@@ -27,7 +31,7 @@ mixin QrManager {
       ..style.width = '100%'
       ..style.height = '100%';
 
-    final CanvasElement canvas = CanvasElement();
+    final web.HTMLCanvasElement canvas = web.HTMLCanvasElement();
     final QRCodeReader reader = QRCodeReader();
     final Completer<String?> completer = Completer<String?>();
 
@@ -40,14 +44,14 @@ mixin QrManager {
       canvas.width = video.videoWidth;
       canvas.height = video.videoHeight;
 
-      final CanvasRenderingContext2D ctx = canvas.context2D;
+      final web.CanvasRenderingContext2D ctx = canvas.context2D;
       ctx.drawImage(video, 0, 0);
 
-      final ImageData imageData =
-          ctx.getImageData(0, 0, canvas.width!, canvas.height!);
+      final web.ImageData imageData =
+          ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-      final LuminanceSource source = luminanceSourceFromImageData(
-          imageData, canvas.width!, canvas.height!);
+      final LuminanceSource source =
+          luminanceSourceFromImageData(imageData, canvas.width, canvas.height);
 
       final BinaryBitmap bitmap = BinaryBitmap(HybridBinarizer(source));
 
@@ -87,24 +91,27 @@ mixin QrManager {
     );
 
     timer.cancel();
-    video.srcObject
-        ?.getTracks()
-        .forEach((MediaStreamTrack track) => track.stop());
+    final web.MediaStream? srcStream = video.srcObject as web.MediaStream?;
+    if (srcStream != null) {
+      srcStream.getTracks().toDart.forEach((web.MediaStreamTrack track) {
+        track.stop();
+      });
+    }
 
     return result ?? await completer.future;
   }
 
   static LuminanceSource luminanceSourceFromImageData(
-      ImageData imageData, int width, int height) {
-    final Uint8ClampedList data = imageData.data;
-    final int length = data.length;
+      web.ImageData imageData, int width, int height) {
+    final JSUint8ClampedArray data = imageData.data;
+    final int length = data.toDart.length;
     final Int32List pixels = Int32List(length ~/ 4);
 
     for (int i = 0, j = 0; i < length; i += 4, j++) {
-      final int r = data[i];
-      final int g = data[i + 1];
-      final int b = data[i + 2];
-      final int a = data[i + 3];
+      final int r = data.toDart[i];
+      final int g = data.toDart[i + 1];
+      final int b = data.toDart[i + 2];
+      final int a = data.toDart[i + 3];
       pixels[j] = (a << 24) | (r << 16) | (g << 8) | b; // AARRGGBB
     }
 

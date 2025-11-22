4c9ebb0ff4d24f82db1c049bcbf24c006e9e1687
vjrj <vjrj@comunes.org>
2025-11-22 10:22:25 +0100

    Remove suffix in card name

--- lib/ui/widgets/first_screen/simple_card_name_text.dart
+++ lib/ui/widgets/first_screen/simple_card_name_text.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../../data/models/stored_account.dart';
-import '../../ui_helpers.dart';
 import '../card_helper.dart';
 import 'card_text_style.dart';
 
@@ -33,11 +32,12 @@ class SimpleCardNameText extends StatelessWidget {
                       text: currentText,
                       style: cardTextStyle(context, fontSize: 15),
                     ),
-                  if (addSuffix && account.type == AccountType.v1PasswordLess)
+                  /*// Show g1nkgo suffix only in v1 passwordLess accounts
+                  if (account.type == AccountType.v1PasswordLess)
                     TextSpan(
                       text: g1nkgoUserNameSuffix,
                       style: cardTextStyle(context, fontSize: 15),
-                    ),
+                    ),*/
                 ],
               ),
               maxLines: 2,

2e2982b5165e53833ef0c78a558a99c016828feb
vjrj <vjrj@comunes.org>
2026-02-28 23:31:36 +0100

    Remove unnecesario perm

--- lib/ui/notification_controller_mobile.dart
+++ lib/ui/notification_controller_mobile.dart
@@ -271,47 +271,6 @@ class NotificationController {
     return;
   }
 
-  static Future<void> scheduleNewNotification() async {
-    if (!kIsWeb && Platform.isLinux) {
-      debugPrint(
-          '[NOTIFICATION] scheduleNewNotification not supported on Linux');
-      return;
-    }
-    bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
-    if (!isAllowed) {
-      isAllowed = await displayNotificationRationale();
-    }
-    if (!isAllowed) {
-      return;
-    }
-
-    await AwesomeNotifications().createNotification(
-        content: NotificationContent(
-            id: -1,
-            // -1 is replaced by a random number
-            channelKey: 'alerts',
-            title: 'Huston! The eagle has landed!',
-            body:
-                "A small step for a man, but a giant leap to Flutter's community!",
-            bigPicture: 'https://storage.googleapis.com/cms-storage-bucket/d406c736e7c4c57f5f61.png',
-            largeIcon: 'https://storage.googleapis.com/cms-storage-bucket/0dbfcc7a59cd1cf16282.png',
-            //'asset://assets/images/balloons-in-sky.jpg',
-            notificationLayout: NotificationLayout.BigPicture,
-            payload: <String, String>{
-              'notificationId': '1234567890'
-            }),
-        actionButtons: <NotificationActionButton>[
-          NotificationActionButton(key: 'REDIRECT', label: 'Redirect'),
-          NotificationActionButton(
-              key: 'DISMISS',
-              label: 'Dismiss',
-              actionType: ActionType.DismissAction,
-              isDangerousOption: true)
-        ],
-        schedule: NotificationCalendar.fromDate(
-            date: DateTime.now().add(const Duration(seconds: 10))));
-  }
-
   /// Check if notifications are currently allowed (permission granted).
   ///
   /// Returns: true if notification permission is granted, false otherwise

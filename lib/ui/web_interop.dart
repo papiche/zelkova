4572a5a7894880ae8b9ad394bc29b194f127811c
vjrj <vjrj@comunes.org>
2026-03-09 16:49:58 +0100

    Fix for old Android

---
+++ lib/ui/web_interop.dart
@@ -0,0 +1,12 @@
+// Conditional imports for web interop
+// This file automatically imports the correct implementation based on platform
+
+import 'package:flutter/foundation.dart';
+
+// Conditional import
+import 'web_interop_web.dart' if (dart.library.io) 'web_interop_stub.dart'
+    as web_interop;
+
+/// Get the current window URL href
+/// Returns empty string on non-web platforms
+String getWindowLocationHref() => web_interop.getWindowLocationHref();
